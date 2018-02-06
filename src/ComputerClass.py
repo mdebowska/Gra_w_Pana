834cf2a23fb765767f8d7e31c4832acf89fed624
Małgorzata Dębowska <Magorzata Dbowska>
2018-02-06 13:57:35 +0100

    game with computer easy

--- src/ComputerClass.py
+++ src/ComputerClass.py
@@ -6,3 +6,40 @@ class Computer(PersonClass.Person):
     """
     def __repr__(self):
         return 'Komputer %s' % self.name
+
+    def sort_hand(self):
+        """
+        Poukładaj po kolei karty w ręce
+        :return:
+        """
+        self.hand = sorted(self.hand, key=lambda item: item.value)
+
+    def choose_cards_to_lay(self, game):
+        """
+        Wybiera karty najlepsze do zagrania
+        :return: najmniejszą kartę możliwą do zagrania lub -1 jeśli takiej nie ma
+        """
+        for id in range(len(self.hand)):
+            if self.hand[id].value >= game.stack[len(game.stack)-1].value:
+                print('value;', self.hand[id].value)
+                return [id]
+
+        return -1
+    
+    #
+    # def choose_cards_to_lay(self, game):
+    #     """
+    #     Wybiera karty najlepsze do zagrania
+    #     :return: najmniejszą kartę możliwą do zagrania lub -1 jeśli takiej nie ma
+    #     """
+    #     cards_id=[]
+    #     for id in range(len(self.hand)):
+    #         if self.hand[id].value >= game.stack[len(game.stack)-1].value:
+    #             print('value;', self.hand[id].value)
+    #             if cards_id != []:
+    #                 if self.hand[id].value == cards_id[0]:
+    #                     cards_id.append(id)
+    #             else:
+    #                 cards_id.append(id)
+    #     return cards_id
+    #     # return -1
--- src/route.py
+++ src/route.py
@@ -107,8 +107,32 @@ def play_game():
         next_label = 0 #nie wiadomo, czy ktoś rzucił już kartami, więc domyślnie jest 0
 
     active_player.set_useful_cards(game)
+    if type(active_player)==ComputerClass.Computer:
+        active_player.sort_hand()
+        if active_player.choose_cards_to_lay(game) != -1:
+            print('próbuję', active_player, active_player.choose_cards_to_lay(game))
+            cards = []
+
+            for id in active_player.choose_cards_to_lay(game):
+                cards.append(active_player.hand[int(id)])
+            # print('Cards: ', cards)
+
+            check_cards = []
+            check_cards.extend(cards)
+            check_cards.extend(active_player.layed_card)
+            # print('Sprawdź te karty:', check_cards)
+            if game.check_if_good_to_add(check_cards):
+                game.add_to_stack(cards, active_player)
+                return redirect(url_for('play_next'))
+        else:
+            return redirect(url_for('play_take_from_stack'))
+
+
+
+
     card_useful_list = active_player.make_useful_list()
     print(card_useful_list)
+
     if request.form.getlist('card'):    #jeśli pobrano listę kart do zagrania to:
         cards = []
 
@@ -132,6 +156,59 @@ def play_game():
 
 
     return render_template('game.html', players=game.players, a_player = active_player, stack = game.stack, next_label = next_label, card_useful = card_useful_list)
+
+
+
+# @app.route('/game/play/computer', methods=['GET','POST'])
+# def play_computer():
+#     """
+#     Turn page
+#     :return:
+#     """
+#
+#     active_player = game.find_active_player()
+#
+#     if game.stack == []:    #sam początek gry
+#         game.add_to_stack([CardClass._9s], active_player)
+#         if active_player.layed_card:
+#             next_label = 1
+#             # print("położyłem kartę, patrz: ", active_player.layed_card)
+#         # else:
+#             # print("nic nie dałem")
+#     else:
+#         next_label = 0 #nie wiadomo, czy ktoś rzucił już kartami, więc domyślnie jest 0
+#
+#     active_player.set_useful_cards(game)
+#     card_useful_list = active_player.make_useful_list()
+#     print(card_useful_list)
+#     if request.form.getlist('card'):    #jeśli pobrano listę kart do zagrania to:
+#         cards = []
+#
+#         for id in request.form.getlist('card'):
+#             cards.append(active_player.hand[int(id)])
+#         # print('Cards: ', cards)
+#
+#         check_cards = []
+#         check_cards.extend(cards)
+#         check_cards.extend(active_player.layed_card)
+#         # print('Sprawdź te karty:', check_cards)
+#         if game.check_if_good_to_add(check_cards):
+#             game.add_to_stack(cards, active_player)
+#             return redirect(url_for('play_next'))
+#         # else:
+#         #     print('NIE ZGADZAM SIĘ!')
+#
+#     if active_player.layed_card != []:
+#         next_label = 1  # informacja, że karty zostały rzucone => można wyświetlić przycisk "zakończ"
+#
+#
+#
+#     return render_template('game.html', players=game.players, a_player = active_player, stack = game.stack, next_label = next_label, card_useful = card_useful_list)
+#
+
+
+
+
 
 @app.route('/game/human/next', methods=['GET','POST'])
 def play_next():
@@ -164,13 +241,14 @@ def play_take_from_stack():
     Page to take cards from stack
     :return:
     """
-
+    print("ciagnie", game.find_active_player())
     if request.form.get('from_stack'):
         game.take_from_stack(int(request.form.get('from_stack')), game.find_active_player())
-        game.swich_active_person()
-    # else:
-        # print('coś poszło nie tak')
+    else: #gra komputer
+        print("ciagnie kompyuter", game.find_active_player())
+        game.take_from_stack(3, game.find_active_player())
 
+    game.swich_active_person()
 
     return redirect(url_for('play_game'))
 
